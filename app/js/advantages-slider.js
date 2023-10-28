2b87d32fe894724c1f07021aed0fcfffca9a3d10
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-10-28 18:13:46 +0300

    add section hero

--- app/js/advantages-slider.js
+++ app/js/advantages-slider.js
@@ -1,14 +1,24 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 'auto',
+    // slidesPerView: 'auto',
     spaceBetween: 30,
     freeMode: true,
     breakpoints: {
-
         320: {
-
             slidesPerView: 'auto',
             spaceBetween: 30,
         },
-
+        768: {
+            slidesPerView: 1.5,
+        },
+        1240: {
+            slidesPerView: 2.5,
+        },
+        1440: {
+            slidesPerView: 'auto',
+        },
+        1920: {
+            slidesPerView: 3,
+        },
     }
 });
+
