a3e43605f7de6d3acb41045b672ed4a044c4ee7c
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-10-29 15:36:36 +0200

    1

--- app/js/slider-3.js
+++ app/js/slider-3.js
@@ -1,26 +1,55 @@
-const swiper3 = new Swiper('.swiper-reviews', {
-    spaceBetween: 30,
-    autoplay: {
-        delay: 3000,
-    },
-    freeMode: true,
-    breakpoints: {
-        // 320: {
-        //     slidesPerView: 'auto',
-        //     spaceBetween: 30,
-        // },
-        768: {
-            slidesPerView: 1.5,
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper1 = new Swiper('.swiper-advantages', {
+        // slidesPerView: 'auto',
+        spaceBetween: 30,
+        autoplay: {
+            delay: 3000,
         },
-        1240: {
-            slidesPerView: 2.5,
-        },
-        1440: {
-            slidesPerView: 3,
-        },
-        1920: {
-            slidesPerView: 3,
-        },
-    }
-});
+        freeMode: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 'auto',
+                spaceBetween: 30,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1240: {
+                slidesPerView: 2,
+            },
+            1440: {
+                slidesPerView: 2,
+            },
+            1920: {
+                slidesPerView: 2,
+            },
+        }
+    });
 
+
+    const swiper2 = new Swiper('.swiper-reviews', {
+        spaceBetween: 30,
+        autoplay: {
+            delay: 3000,
+        },
+        freeMode: true,
+        breakpoints: {
+            320: {
+                slidesPerView: null,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1240: {
+                slidesPerView: 2,
+            },
+            1440: {
+                slidesPerView: 2,
+            },
+            1920: {
+                slidesPerView: 2,
+            },
+        }
+    });
+
+})
